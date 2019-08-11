2aada0ad1e6db7be895307c661176fecfc34cff2
Pedro <plpires99@gmail.com>
2019-08-11 23:52:19 +0100

    Reee-updated the CSS
    + Added more space for the user to gaze and navigate thru

---
+++ assets/Javascript/hoho.js
@@ -0,0 +1,13 @@
+var windowHeightScrollMinY = $(this).height() - 200;
+
+$(window).scroll(function() {
+  var x = $(this).scrollTop();
+  console.log(x);
+  if (x >= windowHeightScrollMinY) {
+    $(".site-header").css("background", "#222222");
+    $(".site-header").css("padding-top", "0px");
+  } else {
+    $(".site-header").css("background", "#22222200");
+    $(".site-header").css("padding-top", "20px");
+  }
+});
